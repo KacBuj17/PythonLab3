b7d660f1fc6d3666300a87473511161c1ad531a3
KacperBujak <83590634+KacBuj17@users.noreply.github.com>
2024-12-13 13:39:54 +0100

    Update aho_corasick.py

--- aho_corasick.py
+++ aho_corasick.py
@@ -1,9 +1,8 @@
 from collections import deque, defaultdict
 
-
 class AhoCorasick:
     def __init__(self):
-        self.trie = {}
+        self.trie = {0: {}}
         self.fail = {}
         self.output = defaultdict(list)
         self.state_count = 0
@@ -11,37 +10,36 @@ class AhoCorasick:
     def add_pattern(self, input_pattern):
         state = 0
         for char in input_pattern:
-            if char not in self.trie.get(state, {}):
-                self.trie.setdefault(state, {})[char] = self.state_count + 1
+            if char not in self.trie[state]:
                 self.state_count += 1
+                self.trie[state][char] = self.state_count
+                self.trie[self.state_count] = {}
             state = self.trie[state][char]
         self.output[state].append(input_pattern)
 
     def build(self):
         queue = deque()
-        for char, next_state in self.trie.get(0, {}).items():
+        for char, next_state in self.trie[0].items():
             self.fail[next_state] = 0
             queue.append(next_state)
         while queue:
             state = queue.popleft()
-            for char, next_state in self.trie.get(state, {}).items():
-                queue.append(next_state)
-                fail_state = self.fail.get(state, 0)
-                while fail_state and char not in self.trie.get(fail_state, {}):
-                    fail_state = self.fail.get(fail_state, 0)
-                self.fail[next_state] = self.trie.get(fail_state, {}).get(char, 0)
+            for char, next_state in self.trie[state].items():
+                fail_state = self.fail[state]
+                while fail_state and char not in self.trie[fail_state]:
+                    fail_state = self.fail[fail_state]
+                self.fail[next_state] = self.trie[fail_state].get(char, 0)
                 self.output[next_state].extend(self.output[self.fail[next_state]])
+                queue.append(next_state)
 
     def search(self, input_text):
         state = 0
         results = []
         for i, char in enumerate(input_text):
-            while state and char not in self.trie.get(state, {}):
+            while state and char not in self.trie[state]:
                 state = self.fail[state]
-            state = self.trie.get(state, {}).get(char, 0)
-            if self.output[state]:
-                for pattern in self.output[state]:
-                    results.append((i - len(pattern) + 1, pattern))
+            state = self.trie[state].get(char, 0)
+            results.extend((i - len(pattern) + 1, pattern) for pattern in self.output[state])
         return results
 
 
